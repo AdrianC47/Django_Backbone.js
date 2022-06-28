76674c90d6ab5beb0c0d79dee261ed8e06fddd25
Adrian <luiscabreraucme@hotmail.com>
2022-06-28 11:11:44 -0500

    Manejo de Views en Django parte 2

--- sap/webapp/views.py
+++ sap/webapp/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 def bienvenido(request):
     return HttpResponse('Hola mundo desde Django')
+
+def despedirse(request):
+    return  HttpResponse('Eso es todo amigos')
+
+def contacto(request):
+    return HttpResponse('Luis Adrián Cabrera - lcabrerab@est.ups.edu.ec')
