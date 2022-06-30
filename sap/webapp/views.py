d2ffea9ef7a6764263c7606eef866ac008671cc5
Adrian <luiscabreraucme@hotmail.com>
2022-06-29 23:45:27 -0500

    Desplegando información dinámica  en un Template en Django

--- sap/webapp/views.py
+++ sap/webapp/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def bienvenido(request):
+    mensajes = {'msg1':'Valor mensaje 1', 'msg2':'Valor mensaje 2'}
     #return HttpResponse('Hola mundo desde Django')
-    return render(request, 'bienvenido.html')
+    return render(request, 'bienvenido.html', mensajes)
 def despedirse(request):
     return  HttpResponse('Eso es todo amigos')
 
