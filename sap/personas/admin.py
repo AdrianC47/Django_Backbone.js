25fac351b1a88c1f44582bd231336764b03fce02
AdrianC47 <59811924+AdrianC47@users.noreply.github.com>
2022-07-01 02:26:01 -0500

    Delete sap directory

--- sap/personas/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-#Aqui registro mis modelos
-from personas.models import Persona, Domicilio
-
-admin.site.register(Persona)
-admin.site.register(Domicilio)
